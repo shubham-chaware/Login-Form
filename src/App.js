947f6ec9f86cd33696afbc467df699117f598cb8
shubham-chaware <chawares26@gmail.com>
2024-06-17 23:21:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './componuts/asstets/LoginForm/LoginForm';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+     <form action="">
+      <h1>Login</h1>
+      <div className="input-box">
+        <input type="text" placeholder='Username' required />
+      </div>
+      <div className="input-box">
+        <input type="password" placeholder='Password' required />
+      </div>
+
+
+      <div className="remember-forget">
+        <label> <input type ="checkbox" />Remember me</label>
+        <a href="#">Forget Password?</a>
+      </div>
+
+      <button type='submit'>Login</button>
+
+      <div className='registor-link'>
+        <p>Don't have an account <a href="#">Registor</a></p>
+      </div>
+     </form>
     </div>
   );
 }
---
+++ src/componuts/asstets/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './LoginForm.css';
+const LoginForm = () =>{
+    return(
+        <div>
+            LoginForm
+        </div>
+    );
+};
+
+export default LoginForm;
